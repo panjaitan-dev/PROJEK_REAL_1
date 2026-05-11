9d8006b440ee864d93a8c8f539ffff70e8031926
panjaitan-dev <249464640+panjaitan-dev@users.noreply.github.com>
2026-05-11 16:21:42 +0700

    .

--- app/Http/Controllers/InformasiController.php
+++ app/Http/Controllers/InformasiController.php
@@ -9,7 +9,7 @@ class InformasiController extends Controller
     public function index()
     {
         $sejarahList = Informasi::where('status', 1)
-            ->orderBy('id', 'asc')
+            ->orderBy('urutan', 'asc')
             ->get();
         
         return view('pages.informasi', compact('sejarahList'));
